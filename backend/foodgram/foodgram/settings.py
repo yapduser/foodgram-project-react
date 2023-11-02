65d1238a40fb506c17c7c6447329ecee019eee98
yapduser <apronyaev@yandex.ru>
2023-11-03 01:20:01 +0300

    Формат

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -112,7 +112,6 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ],
-    # TODO: Проверить работу дял всего контента.
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 6,
 }
