7ef3e59dc30e349f918cf33873b3e23b61c182ac
yapduser <apronyaev@yandex.ru>
2023-11-03 00:52:24 +0300

    Формат

--- backend/foodgram/recipes/management/commands/load_data_csv.py
+++ backend/foodgram/recipes/management/commands/load_data_csv.py
@@ -9,8 +9,8 @@ logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        with open('data/ingredients.csv', 'r', encoding='utf-8') as file:
-            reader = csv.reader(file, delimiter=',')
+        with open("data/ingredients.csv", "r", encoding="utf-8") as file:
+            reader = csv.reader(file, delimiter=",")
             ingredients_to_create = []
             for row in reader:
                 name, unit = row
@@ -18,4 +18,3 @@ class Command(BaseCommand):
                     ingredient = Ingredient(name=name, measurement_unit=unit)
                     ingredients_to_create.append(ingredient)
             Ingredient.objects.bulk_create(ingredients_to_create)
-
