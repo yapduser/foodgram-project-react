a2f9f933fa4b10fe6f009a5903265b039e7d1490
yapduser <apronyaev@yandex.ru>
2023-11-02 21:42:11 +0300

    Добавлена загрузка ингредиентов

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # praktikum_new_diplom
+
+
+Загрузить данные
+```shell
+python manage.py load_data_csv
+
+```
--- backend/foodgram/up.sh
+++ backend/foodgram/up.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-ADMIN_USERNAME=admin
-ADMIN_EMAIL=admin@admin.com
-ADMIN_PASSWORD=1111
-
 # Выполнить миграции
 python manage.py migrate
-
-# Создать суперпользователя
-echo "from django.contrib.auth import get_user_model; User = get_user_model();
-User.objects.create_superuser('$ADMIN_USERNAME', '$ADMIN_EMAIL', '$ADMIN_PASSWORD')" | python manage.py shell
 
 # Создать теги
 add_tag() {
@@ -17,8 +9,6 @@ add_tag() {
     color="$2"
     slug="$3"
 
-    # <recipes.models> - заменить на выш путь к файлу моделей.
-    # <Tag> - заменить на имя вашего класса тегов.
     echo "from recipes.models import Tag;
 Tag.objects.create(name='$name', color='$color', slug='$slug')" | python manage.py shell
 }
@@ -26,18 +16,8 @@ add_tag "Завтрак" "#FF0000" "breakfast"
 add_tag "Обед" "#00FF00" "lunch"
 add_tag "Ужин" "#0000FF" "dinner"
 
-# Создать ингредиент
-add_ingredient() {
-    name="$1"
-    measurement_unit="$2"
-
-    # <recipes.models> - заменить на выш путь к файлу моделей.
-    # <Ingredient> - заменить на имя вашего класса ингредиентов.
-    echo "from recipes.models import Ingredient;
-Ingredient.objects.create(name='$name', measurement_unit='$measurement_unit')" | python manage.py shell
-}
-add_ingredient "Капуста" "кг"
-add_ingredient "Молоко" "л"
+# Запустить ингредиент
+python manage.py load_data_csv
 
 # Запустить сервер
 python manage.py runserver
