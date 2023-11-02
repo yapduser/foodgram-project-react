606b02d7b0a1eff4390bda774e43e4cdfa7ee88e
yapduser <apronyaev@yandex.ru>
2023-11-03 01:21:37 +0300

    0

--- backend/foodgram/up.sh
+++ backend/foodgram/up.sh
@@ -6,7 +6,6 @@ python manage.py collectstatic
 # Выполнить миграции
 python manage.py migrate
 
-# TODO: Удалить
 # Создать суперпользователя
 echo "from django.contrib.auth import get_user_model; User = get_user_model();
 User.objects.create_superuser('$ADMIN_USERNAME', '$ADMIN_EMAIL', '$ADMIN_PASSWORD')" | python manage.py shell
