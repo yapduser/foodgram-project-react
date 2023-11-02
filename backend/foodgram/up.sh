6b7ab229a7307082162daba48fce01734062020e
yapduser <apronyaev@yandex.ru>
2023-11-02 23:39:20 +0300

    Локальный билд

--- backend/foodgram/up.sh
+++ backend/foodgram/up.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Собрать статику
+python manage.py collectstatic
+
 # Выполнить миграции
 python manage.py migrate
+
+# TODO: Удалить
+# Создать суперпользователя
+echo "from django.contrib.auth import get_user_model; User = get_user_model();
+User.objects.create_superuser('$ADMIN_USERNAME', '$ADMIN_EMAIL', '$ADMIN_PASSWORD')" | python manage.py shell
 
 # Создать теги
 add_tag() {
@@ -16,8 +24,8 @@ add_tag "Завтрак" "#FF0000" "breakfast"
 add_tag "Обед" "#00FF00" "lunch"
 add_tag "Ужин" "#0000FF" "dinner"
 
-# Запустить ингредиент
+# Загрузить ингредиент
 python manage.py load_data_csv
 
 # Запустить сервер
-python manage.py runserver
+gunicorn --bind 0.0.0.0:8000 foodgram.wsgi
