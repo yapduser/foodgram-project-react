7f2ac0dbd2a95b5d832826d31cbef1feb489dde2
yapduser <apronyaev@yandex.ru>
2023-11-08 22:53:27 +0300

    0

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -160,7 +160,7 @@ class Recipe(models.Model):
     class Meta:
         verbose_name = "Рецепт"
         verbose_name_plural = "Рецепты"
-        ordering = ["name"]
+        ordering = ["-id"]
 
     def __str__(self):
         return self.name
